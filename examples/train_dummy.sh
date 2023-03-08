45bf4ba4e35fd419bb504fa093b6f21430999cba
2059746@qq.com <2059746@qq.com>
2023-03-08 22:43:52 +0800

    重构业务系统

--- examples/train_dummy.sh
+++ examples/train_dummy.sh
@@ -1,13 +1,14 @@
 set_n_least_used_CUDA_VISIBLE_DEVICES() {
     local n=${1:-"9999"}
     echo "GPU Memory Usage:"
-    local FIRST_N_GPU_IDS=$(nvidia-smi --query-gpu=memory.used --format=csv \
-        | tail -n +2 \
-        | nl -v 0 \
-        | tee /dev/tty \
-        | sort -g -k 2 \
-        | awk '{print $1}' \
-        | head -n $n)
+#    local FIRST_N_GPU_IDS=$(nvidia-smi --query-gpu=memory.used --format=csv \
+#        | tail -n +2 \
+#        | nl -v 0 \
+#        | tee /dev/tty \
+#        | sort -g -k 2 \
+#        | awk '{print $1}' \
+#        | head -n $n)
+    local FIRST_N_GPU_IDS = '0'
     export CUDA_VISIBLE_DEVICES=$(echo $FIRST_N_GPU_IDS | sed 's/ /,/g')
     echo "Now CUDA_VISIBLE_DEVICES is set to:"
     echo "CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES"
